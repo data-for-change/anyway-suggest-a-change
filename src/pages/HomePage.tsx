0358e1494d980540fbc9736cf989d63ffc38d4b1
yuval hazony <hazyuval@gmail.com>
2023-08-16 21:56:28 +0300

    feat(homePage): search location

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
-import { Box, makeStyles, Theme } from '@material-ui/core';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Box, Button, makeStyles, Theme, Typography } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
+import MapDialog from 'components/molecules/MapDialog';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { IPoint } from 'models/Point';
+import RootStore from 'store/root.store';
+import { useStore } from 'store/storeConfig';
 
 import StreetCard, { StreetCardProps } from 'components/StreetCard';
 import ResponsiveDrawer from 'components/molecules/infoDrawer/Drawer';
 import SectionInfo from 'components/molecules/sectionInfo';
 
+enum LocationType {
+  cityAndStreet,
+  sagment
+}
+
+type Location = {
+  type: LocationType,
+  location: string,
+  id?: string
+}
+
 const HomePage = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const store: RootStore = useStore();
+
+  const { settingsStore } = store;
+
+  const [open, setOpen] = useState(false);
+
+  const [currentLocation, setCurrentLocation] = useState<Location>({ type: LocationType.cityAndStreet, location: 'תל אביב, אלנבי' })
 
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [selectedSection, setSelectedSection] = useState({});
@@ -15,7 +41,6 @@ const HomePage = () => {
   const handleDrawerToggle = () => {
     setIsDrawerOpen(!isDrawerOpen);
   };
-
 
   const [cards, setCards] = useState<StreetCardProps[]>([
     { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
@@ -29,15 +54,58 @@ const HomePage = () => {
     { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
   ]);
 
+  const roadSegmentLocation = store.gpsLocationData;
+
+  const onLocationChange = useCallback(
+    (location: IPoint) => {
+      store.fetchGpsLocation(location);
+    },
+    [store],
+  );
+
+  const onLocationSearch = () => {
+    if (roadSegmentLocation) {
+      //navigate(`${settingsStore.currentLanguageRouteString}/location/${roadSegmentLocation?.road_segment_id}`);
+      setCurrentLocation({ type: LocationType.sagment, location: roadSegmentLocation.road_segment_name, id: roadSegmentLocation.road_segment_id.toString() })
+      setOpen(false);
+      store.setGpsLocationData(null);
+    }
+  };
+
+  const onStreetAndCitySearch = (street?: string, city?: string) => {
+    // change to constant values until backend issues are fixed
+    console.log('city is', city);
+    console.log('street is', street);
+    //navigate(`${settingsStore.currentLanguageRouteString}/cityAndStreet/${street}/${city}`);
+    setCurrentLocation({ type: LocationType.cityAndStreet, location: `${street}, ${city}` })
+    setOpen(false);
+  };
+
   return (
     <Box className={classes.container}>
       <Box className={classes.columnContainer}>
-        {cards.map((streetData, index) => (
+        <Typography>
+          {currentLocation.location} - <Button onClick={() => setOpen(true)}>שינוי כתובת</Button>
+        </Typography>
+        {cards.map((streetData: StreetCardProps, index: number) => (
           <StreetCard key={index} {...streetData} />
         ))}
+
+        <MapDialog
+          open={open}
+          section={roadSegmentLocation?.road_segment_name}
+          roadNumber={roadSegmentLocation?.road1}
+          onLocationChange={onLocationChange}
+          onClose={() => {
+            setOpen(false);
+            store.setGpsLocationData(null);
+          }}
+          onSearch={onLocationSearch}
+          onStreetAndCitySearch={onStreetAndCitySearch}
+        />
       </Box>
       <Box className={classes.columnContainer}>
-          <SectionInfo section={selectedSection}/>
+        <SectionInfo section={selectedSection} />
       </Box>
       {/* <ResponsiveDrawer isDrawerOpen={isDrawerOpen}>
 
