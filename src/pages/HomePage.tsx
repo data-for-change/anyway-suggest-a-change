68da99f15dd27e1a4043c9c2c98050d6ccafb2de
Amit Shalev <amitshalev@h-MacBook-Pro-sl-Amit.local>
2023-08-16 09:47:14 +0300

    copy all from their repo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,76 +1,45 @@
-import { useEffect, FC } from 'react';
-import { Box } from '@material-ui/core';
-import SideBar from 'components/organisms/SideBar';
-import makeStyles from '@material-ui/core/styles/makeStyles';
-import { silverSmokeColor } from '../style';
-import FilterBar from 'components/organisms/FilterBar';
-import OverlayLoader from 'components/molecules/OverlayLoader';
+import { useState } from 'react';
+import StreetCard, { StreetCardProps } from 'components/StreetCard';
+import { Box, makeStyles, Theme } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
-import { useStore } from 'store/storeConfig';
-import RootStore from 'store/root.store';
-import { Navigate, useParams, Outlet } from 'react-router-dom';
-import { IRouteProps } from 'models/Route';
-import { LANG } from 'const/languages.const';
 
-interface IProps {}
-
-const useStyles = makeStyles({
-  mainBox: {
-    height: 'inherit',
-  },
-
-  widgetBox: {
-    height: 'inherit',
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: '24px',
+    justifyContent: 'center',
     overflow: 'auto',
   },
-  sideBarWrapper: {
-    borderInlineEnd: `1px solid ${silverSmokeColor}`,
-  },
-});
+}));
 
-const HomePage: FC<IProps> = () => {
+function HomePage() {
   const classes = useStyles();
-  const store: RootStore = useStore();
-  const { newsFlashStore, widgetsStore, settingsStore } = store;
-  const { gpsId, newsId, lng, city, street } = useParams<IRouteProps>();
-  const loading = widgetsStore.widgetBoxLoading;
 
-  useEffect(() => {
-    if (city && street) {
-      newsFlashStore.selectNewsFlashByCityAndStreet(city, street);
-    }
-    if (newsId) {
-      newsFlashStore.selectNewsFlash(parseInt(newsId));
-    }
-    if (gpsId) {
-      newsFlashStore.selectLocationId(parseInt(gpsId));
-    }
-  }, [newsId, newsFlashStore, gpsId, city, street]);
-
-  useEffect(() => {
-    if (lng) {
-      settingsStore.changeLanguage(lng);
-    } else {
-      settingsStore.changeLanguage(LANG.HE);
-    }
-  }, [lng, settingsStore]);
-
-  if (!newsId && !gpsId && !street && !city) {
-    return <Navigate to="/" replace />;
-  }
+  const [cards, setCards] = useState<StreetCardProps[]>([
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+    { streetName: 'בוגרשוב', city: 'תל אביב' },
+  ]);
 
   return (
-    <Box display="flex" flexGrow={1} className={classes.mainBox}>
-      <Box flexGrow={1} width={380} display="flex" className={classes.sideBarWrapper}>
-        <SideBar />
-      </Box>
-      <Box flexGrow={5} className={classes.widgetBox} position="relative">
-        <OverlayLoader show={loading} />
-        <FilterBar />
-        <Outlet />
-      </Box>
+    <Box className={classes.container}>
+      {cards.map((streetData, index) => (
+        <StreetCard key={index} {...streetData} />
+      ))}
     </Box>
   );
-};
+}
 
 export default observer(HomePage);
