7195e11562f85b59b29244c14631ccf9e239edd7
Alon Mantin <alonmantin@Alons-MacBook-Pro.local>
2023-08-16 17:00:13 +0300

    created template for section for infographics and comments

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,41 +1,65 @@
-import { useState } from 'react';
-import StreetCard, { StreetCardProps } from 'components/StreetCard';
+import React, { useState } from 'react';
 import { Box, makeStyles, Theme } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
+
+import StreetCard, { StreetCardProps } from 'components/StreetCard';
+import ResponsiveDrawer from 'components/molecules/infoDrawer/Drawer';
+import SectionInfo from 'components/molecules/sectionInfo';
+
+const HomePage = () => {
+  const classes = useStyles();
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [selectedSection, setSelectedSection] = useState({});
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+
+  const [cards, setCards] = useState<StreetCardProps[]>([
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+    { streetName: 'בוגרשוב', city: 'תל אביב', handleClick: handleDrawerToggle },
+  ]);
+
+  return (
+    <Box className={classes.container}>
+      <Box className={classes.columnContainer}>
+        {cards.map((streetData, index) => (
+          <StreetCard key={index} {...streetData} />
+        ))}
+      </Box>
+      <Box className={classes.columnContainer}>
+          <SectionInfo section={selectedSection}/>
+      </Box>
+      {/* <ResponsiveDrawer isDrawerOpen={isDrawerOpen}>
+
+      </ResponsiveDrawer> */}
+    </Box>
+  );
+}
+
+export default observer(HomePage);
 
 const useStyles = makeStyles((theme: Theme) => ({
   container: {
     height: '100%',
     margin: '16px',
     overflow: 'auto',
+    display: 'flex',
+    flexDirection: 'row',
   },
+  columnContainer: {
+    height: '100%',
+    width: '50%',
+    display: 'flex',
+    flexDirection: 'column',
+  }
 }));
-
-function HomePage() {
-  const classes = useStyles();
-
-  const [cards, setCards] = useState<StreetCardProps[]>([
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-    { streetName: 'בוגרשוב', city: 'תל אביב' },
-  ]);
-
-  return (
-    <Box className={classes.container}>
-      {cards.map((streetData, index) => (
-        <StreetCard key={index} {...streetData} />
-      ))}
-    </Box>
-  );
-}
-
-export default observer(HomePage);
